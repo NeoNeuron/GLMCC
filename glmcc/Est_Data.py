dd7b84f2793fdb3c395b748b5e52811943aeeaa8
Kai(AMD256) <kchen513@outlook.com>
2025-08-15 19:47:50 +0800

    add parallisation in Est_Data, and remove the temporal file

--- glmcc/Est_Data.py
+++ glmcc/Est_Data.py
@@ -13,11 +13,99 @@ import numpy as np
 import math
 from pathlib import Path, PosixPath
 from typing import Union
+from multiprocessing import Pool
+SCALE = 1.277
+Z_A = 15.14
+
+def _process_pair(X1: np.ndarray, X2: np.ndarray, T:float, mode: str, LR:bool, beta:float, WIN:float, DELTA:float) -> float:
+    # Make cross-correlogram
+    t0 = time.process_time()
+    print('start computing correlation ...')
+    cc_list = linear_crossCorrelogram(X1, X2, T)
+
+    # set tau
+    tau = [4, 4]
+
+    # Fitting a GLM
+    if mode == 'sim':
+        delay_synapse = 3
+        par, log_pos, log_likelihood = GLMCC(
+            cc_list[1], cc_list[0], tau, beta, cc_list[2], cc_list[3], delay_synapse
+        )
+    elif mode == 'exp':
+        log_pos = 0
+        log_likelihood = 0
+        for m in range(1, 5):
+            tmp_par, tmp_log_pos, tmp_log_likelihood = GLMCC(
+                cc_list[1], cc_list[0], tau, beta, cc_list[2], cc_list[3], m
+            )
+            if m == 1 or (not LR and tmp_log_pos > log_pos) or (LR and tmp_log_likelihood > log_likelihood):
+                log_pos = tmp_log_pos
+                log_likelihood = tmp_log_likelihood
+                par = tmp_par
+                delay_synapse = m
+    else:
+        raise ValueError("Input error: You must write sim or exp in mode")
+
+    # Connection parameters
+    nb = int(WIN / DELTA)
+    cc_0 = [0 for _ in range(2)]
+    max_l = [0 for _ in range(2)]
+    Jmin = [0 for _ in range(2)]
+    for l in range(2):
+        cc_0[l] = 0
+        max_l[l] = int(tau[l] + 0.1)
+
+        if l == 0:
+            for m in range(max_l[l]):
+                cc_0[l] += np.exp(par[nb + int(delay_synapse) + m])
+        if l == 1:
+            for m in range(max_l[l]):
+                cc_0[l] += np.exp(par[nb - int(delay_synapse) - m])
+
+        cc_0[l] = cc_0[l] / max_l[l]
+
+        Jmin[l] = math.sqrt(16.3 / tau[l] / cc_0[l])
+        n12 = tau[l] * cc_0[l]
+        if n12 <= 10:
+            par[NPAR - 2 + l] = 0
+    # calculate W
+    if not LR:
+        Wij = round(calc_PSP(par[NPAR - 1], Jmin[1]*SCALE), 6)
+        Wji = round(calc_PSP(par[NPAR - 2], Jmin[0]*SCALE), 6)
+    else:
+        D1 = 0
+        D2 = 0
+        _, _, log_likelihood_p = GLMCC(
+            cc_list[1], cc_list[0], tau, beta, cc_list[2], cc_list[3], delay_synapse, cond=1
+        )
+        _, _, log_likelihood_n = GLMCC(
+            cc_list[1], cc_list[0], tau, beta, cc_list[2], cc_list[3], delay_synapse, cond=2
+        )
+        D1 = log_likelihood - log_likelihood_p
+        D2 = log_likelihood - log_likelihood_n
+        Wij = round(calc_PSP_LR(par[NPAR - 1], D2, Z_A), 6)
+        Wji = round(calc_PSP_LR(par[NPAR - 2], D1, Z_A), 6)
+
+    #pre : i
+    #post : j
+    #J_+  : par[NPAR - 1]
+    #J_-  : par[NPAR - 2]
+    #J_min_+ : Jmin[1]
+    #J_min_- : Jmin[0]
+    #D_+    : D2
+    #D_-    : D1
+    
+    t1 = time.process_time()
+    elapsed_time = t1 - t0
+
+    return Wij, Wji, elapsed_time
+
 
 def Est_Data(folder: Union[str, PosixPath], DataFileName: str, N: int,
              indices: np.ndarray=None, T: float = 5400, mode: str = 'sim',
              method: str = 'GLM', WIN:float=50.0, DELTA:float=1.0,
-             outfile_sfx:str=None):
+             outfile_sfx:str=None, n_jobs:int=1):
     """
     Estimate synaptic connection strengths (PSPs) between neurons from spike data using GLM or LR methods.
     This function loads spike data, computes cross-correlograms for all neuron pairs, fits a Generalized Linear Model (GLM) or Logistic Regression (LR) to estimate synaptic parameters, and outputs a connectivity matrix as a CSV file.
@@ -44,6 +132,8 @@ def Est_Data(folder: Union[str, PosixPath], DataFileName: str, N: int,
         Bin size for cross-correlogram (default is 1.0).
     outfile_sfx : str, optional
         Suffix for the output file name (default is None, which means no suffix is added).
+    n_jobs : int, optional
+        number of parallel workers
         
     Returns
     -------
@@ -88,109 +178,32 @@ def Est_Data(folder: Union[str, PosixPath], DataFileName: str, N: int,
             input_data[input_data[:, 1] == old_id, 1] = new_id
 
     # Start measuring CPU time and wall time
-    start_cpu_time = time.process_time()
     start_wall_time = time.time()
 
-    cc_time = 0.0
-    for i in range(0, N):
-        for j in range(0, i):
-            X1 = input_data[input_data[:, 1] == i, 0]
-            X2 = input_data[input_data[:, 1] == j, 0]
+    index_pairs = [(i,j) for i in range(N) for j in range(i)]
+    pool = Pool(n_jobs)
+    results = [pool.apply_async(
+        _process_pair, args=(input_data[input_data[:, 1] == i, 0], input_data[input_data[:, 1] == j, 0], T, mode, LR, beta, WIN, DELTA)
+    ) for i, j in index_pairs]
+    pool.close()
+    pool.join()
+    # results_list = []
+    # for i, j in index_pairs:
+    #     # Helper function to process a neuron pair (i -> j)
+    #     X1 = input_data[input_data[:, 1] == i, 0]
+    #     X2 = input_data[input_data[:, 1] == j, 0]
+    #     print(f"Estimating PSPs from node {i} to {j}...")
+    #     results = _process_pair(X1, X2)
+    #     results_list.append((i, j, *results))
 
-            print(f"Estimating PSPs from node {j} to {i}...")
-            #Make cross_correlogram
-            t0 = time.time()
-            cc_list = linear_crossCorrelogram(X1, X2, T)
-            t1 = time.time()
-            cc_time += t1 - t0
-
-            #set tau
-            tau = [4, 4]
-
-            #Fitting a GLM
-            if mode == 'sim':
-                delay_synapse = 3
-                par, log_pos, log_likelihood = GLMCC(cc_list[1], cc_list[0], tau, beta, cc_list[2], cc_list[3], delay_synapse)
-            elif mode == 'exp':
-                log_pos = 0
-                log_likelihood = 0
-                for m in range(1, 5):
-                    tmp_par, tmp_log_pos, tmp_log_likelihood = GLMCC(cc_list[1], cc_list[0], tau, beta, cc_list[2], cc_list[3], m)
-                    if m == 1 or (not LR and tmp_log_pos > log_pos) or (LR and tmp_log_likelihood > log_likelihood):
-                        log_pos = tmp_log_pos
-                        log_likelihood = tmp_log_likelihood
-                        par = tmp_par
-                        delay_synapse = m
-            else:
-                raise ValueError("Input error: You must write sim or exp in mode")
-                        
-
-            #Connection parameters
-            nb = int(WIN/DELTA)
-            cc_0 = [0 for l in range(2)]
-            max = [0 for l in range(2)]
-            Jmin = [0 for l in range(2)]
-            for l in range(2):
-                cc_0[l] = 0
-                max[l] = int(tau[l] + 0.1)
-                
-                if l == 0:
-                    for m in range(max[l]):
-                        cc_0[l] += np.exp(par[nb+int(delay_synapse)+m])
-                if l == 1:
-                    for m in range(max[l]):
-                        cc_0[l] += np.exp(par[nb-int(delay_synapse)-m])
-
-                cc_0[l] = cc_0[l]/max[l]
-                        
-                Jmin[l] = math.sqrt(16.3/ tau[l]/ cc_0[l])
-                n12 = tau[l]*cc_0[l]
-                if n12 <= 10:
-                    par[NPAR-2+l] = 0
-            D1 = 0
-            D2 = 0
-            if LR:
-                tmp_par, tmp_log_pos, log_likelihood_p = GLMCC(cc_list[1], cc_list[0], tau, beta, cc_list[2], cc_list[3], delay_synapse, cond = 1)
-                tmp_par, tmp_log_pos, log_likelihood_n = GLMCC(cc_list[1], cc_list[0], tau, beta, cc_list[2], cc_list[3], delay_synapse, cond = 2)
-                D1 = log_likelihood - log_likelihood_p
-                D2 = log_likelihood - log_likelihood_n
-
-            #Output J
-            # DataFileName.split
-            J_f = open(buff, 'a')
-            J_f.write(str(i)+' '+str(j)+' '
-                    +str(round(par[NPAR-1], 6))+' '+str(round(par[NPAR-2], 6))+' '
-                    +str(round(Jmin[1], 6))+' '+str(round(Jmin[0], 6))+' '
-                    +str(round(D2, 6))+' '+str(round(D1, 6))+'\n')
-            J_f.close()
-
-    scale = 1.277
-    z_a = 15.14
 
     #Read the required J file and create the resul file
-    J_f = open(buff, 'r')
-    J_f_list = J_f.readlines()
     W = np.zeros((N, N))
-
-    #calculate W
-    for i in range(0, len(J_f_list)):
-        J_f_list[i] = J_f_list[i].split()
-        
-        J_f_list[i][0] = int(float(J_f_list[i][0])) #pre
-        J_f_list[i][1] = int(float(J_f_list[i][1])) #post
-        J_f_list[i][2] = float(J_f_list[i][2])      #J_+
-        J_f_list[i][3] = float(J_f_list[i][3])      #J_-
-        J_f_list[i][4] = float(J_f_list[i][4])      #J_min_+
-        J_f_list[i][5] = float(J_f_list[i][5])      #J_min_-
-        J_f_list[i][6] = float(J_f_list[i][6])      #D_+
-        J_f_list[i][7] = float(J_f_list[i][7])      #D_-
-        
-        if not LR:
-            W[J_f_list[i][0], J_f_list[i][1]] = round(calc_PSP(J_f_list[i][2], J_f_list[i][4]*scale), 6)
-            W[J_f_list[i][1], J_f_list[i][0]] = round(calc_PSP(J_f_list[i][3], J_f_list[i][5]*scale), 6)
-        else:
-            W[J_f_list[i][0], J_f_list[i][1]] = round(calc_PSP_LR(J_f_list[i][2], J_f_list[i][6], z_a), 6)
-            W[J_f_list[i][1], J_f_list[i][0]] = round(calc_PSP_LR(J_f_list[i][3], J_f_list[i][7], z_a), 6)
+    cpu_time = 0.0
+    for (i, j), res in zip(index_pairs, results):
+        W[i, j] = res.get()[0]
+        W[j, i] = res.get()[1]
+        cpu_time += res.get()[2]
 
     #write W
     if outfile_sfx is not None:
@@ -199,22 +212,13 @@ def Est_Data(folder: Union[str, PosixPath], DataFileName: str, N: int,
         output_file = folder / f"W_{method:s}_{T:.0f}-{DataFileName:s}.npy"
     np.save(output_file, W)
 
-    #remove J file
-
-    # debug
-    # cmd = ['rm', str(folder/f"J_py_{T:.0f}.txt")]
-    # proc.check_call(cmd)
-
     # End measuring CPU time and wall time
-    end_cpu_time = time.process_time()
     end_wall_time = time.time()
 
     # Calculate the elapsed CPU time and wall time
-    elapsed_cpu_time = end_cpu_time - start_cpu_time
     elapsed_wall_time = end_wall_time - start_wall_time
 
     # Print the elapsed CPU time and wall time
-    print(f"CC estimation time: {cc_time:.3f} s")
-    print(f"Elapsed CPU time: {elapsed_cpu_time:.3f} s")
+    print(f"Elapsed CPU time: {cpu_time:.3f} s")
     print(f"Elapsed Wall time: {elapsed_wall_time:.3f} s")
-    return W, elapsed_cpu_time, elapsed_wall_time
+    return W, cpu_time, elapsed_wall_time
